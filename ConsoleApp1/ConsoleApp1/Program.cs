46128ce414e37499bbb9d78b0aaa58d9c99bcd80
Joe Bhullar <joebhullar11@gmail.com>
2021-10-03 14:59:36 -0600

    ConsoleApp1V3Scope
    Scope V3

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FirstFactorial ninetyFour = new FirstFactorial();
+            int v = ninetyFour.getFirstFactorial("7"); // 7! = 5040
+
+            Console.WriteLine("Hello World!");
+
+            // keep this function call here
+            Console.WriteLine(v);
+            Console.WriteLine("above is my answer for 94!");
+            
+           
+        }
+
+    }
+}
