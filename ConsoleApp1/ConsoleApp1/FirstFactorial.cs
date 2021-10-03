65f267c7e13be2430e095a65bb27f977f1d631c2
Joe Bhullar <joebhullar11@gmail.com>
2021-10-03 14:29:25 -0600

    OuterLayerConsoleApp
    Adding outer Layer of the console app

---
+++ ConsoleApp1/ConsoleApp1/FirstFactorial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class FirstFactorial
+    {
+        public int getFirstFactorial(string v)
+        {
+            int n = int.Parse(v);
+
+            // code goes here
+            int res = 1;
+            while (n != 1)
+            {
+                res = res * n;
+                n = n - 1;
+            }
+            return res;
+        }
+
+        public bool greaterThan1000(int value) 
+        { 
+            if(value > 1000)
+            { return true; }
+            return false;
+        }
+
+        public bool greaterThan3000(int value)
+        {
+            if (value > 3000)
+            { return true; }
+            return false;
+        }
+    }
+
+}
---
+++ ConsoleApp1/ConsoleApp1/SimpleArithmetic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class SimpleArithmetic
+    {
+        public List<string> ArithmeticFunctions = new List<string>() {"Addition", "Subtraction", "Multiplication", "Division"}; 
+        public double AddTwoDoubles(string d1, string d2)
+        {
+            return Convert.ToDouble(d1) + Convert.ToDouble(d2);
+        }
+        public double SubtractTwoDoubles(string d1, string d2)
+        {
+            return Convert.ToDouble(d2) - Convert.ToDouble(d1);
+        }
+        public double MultiplyTwoDoubles(string d1, string d2)
+        {
+            return Convert.ToDouble(d1)*Convert.ToDouble(d2);
+        }
+        public double DivideTwoDoubles(string d1, string d2)
+        {
+            return Convert.ToDouble(d2) / Convert.ToDouble(d1);
+        }
+
+    }
+}
